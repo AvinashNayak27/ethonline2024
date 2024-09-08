de42102021aea348398ee0d0719de96518747e94
Avinash <avinashnayak573@gmail.com>
2024-09-08 14:31:36 +0530

    React router dom setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createThirdwebClient } from "thirdweb";
 import { ThirdwebProvider } from "thirdweb/react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Bets from "./components/Bets.jsx";
+import History from "./components/History.jsx";
+import Dashboard from "./components/dashboard.jsx";
+import Landing from "./components/Landing.jsx";
 export const client = createThirdwebClient({
   clientId: "55a6504e889b8d8708ede6dd50c2f831",
 });
@@ -12,7 +15,14 @@ export const client = createThirdwebClient({
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThirdwebProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/bets" element={<Bets />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </BrowserRouter>
     </ThirdwebProvider>
   </StrictMode>
 );
